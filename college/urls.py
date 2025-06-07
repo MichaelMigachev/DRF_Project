77c620ae62c27409ed3f29e6014f54640ee0288d
MichaelMigachev <migachev21@gmail.com>
2025-06-07 09:36:59 +0300

    Внёс правки к ДЗ 32.1: 1. Функционал работы с подписками закрыл авторизацией. 2. В сериализаторе курса реализовал вывод информации о подписке текущего пользователя через SerializerMethodField

--- college/urls.py
+++ college/urls.py
@@ -19,15 +19,7 @@ router.register(r"course", CourseViewSet, basename="course")
 urlpatterns = [
     path("lesson/crete/", LessonCreteAPIView.as_view(), name="lesson_crete"),
     path("lesson/list/", LessonListAPIView.as_view(), name="lesson_list"),
-    path(
-        "lesson/retriv/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_retriv"
-    ),
-    path(
-        "lesson/update/<int:pk>/", LessonUpdateAPIView.as_view(), name="lesson_update"
-    ),
-    path(
-        "lesson/destroy/<int:pk>/",
-        LessonDestroyAPIView.as_view(),
-        name="lesson_destroy",
-    ),
+    path("lesson/retriv/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_retriv"),
+    path("lesson/update/<int:pk>/", LessonUpdateAPIView.as_view(), name="lesson_update"),
+    path("lesson/destroy/<int:pk>/", LessonDestroyAPIView.as_view(), name="lesson_destroy"),
 ] + router.urls
